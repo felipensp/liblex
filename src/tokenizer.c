eee0edbe688dd4e82ebefa9f3d166a70eb750db7
Felipe Pena <felipensp@gmail.com>
2015-11-20 11:41:25 -0200

    - WS

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,9 +1,9 @@
 /*
  * Tokenizer
- * 
+ *
  * Author: Felipe Pena <felipensp at gmail.com>
  */
- 
+
 #include <string.h>
 #include "liblex.h"
 #include "regex.h"
@@ -32,14 +32,14 @@ static void _llex_set_location(llex *lex, char *buffer, size_t len)
 {
 	lex->buffer_col_start = buffer - lex->current_line_pos;
 	lex->buffer_line_start = lex->current_line;
-	
+
 	for (int i = 0; i < len; ++i) {
 		if (buffer[i] == '\n') {
 			++lex->current_line;
 			lex->current_line_pos = buffer + i;
 		}
 	}
-	
+
 	lex->buffer_col_end = buffer + len - lex->current_line_pos;
 	lex->buffer_line_end = lex->current_line;
 }
@@ -48,14 +48,14 @@ inline static int _llex_find_next_token(llex *lex)
 {
 	lex->current_token = NULL;
 	lex->current_len = 0;
-	
+
 	if (*lex->buffer_pos == '\0') return 0;
-	
+
 	if (lex->options & LLEX_IGNORE_WHITESPACE) {
 		char *buffer_pos = lex->buffer_pos;
-		
+
 		if (!_llex_ltrim(lex)) return 0;
-		
+
 		if (buffer_pos != lex->buffer_pos)
 			_llex_set_location(lex, buffer_pos, lex->buffer_pos - buffer_pos);
 	}
@@ -66,25 +66,27 @@ inline static int _llex_find_next_token(llex *lex)
 		if ((t->type == LLEX_LITERAL_TOKEN || t->type == LLEX_LITERAL_TOKEN_CALLBACK)
 			&& lex->buffer_pos[0] == t->token.literal.str[0]
 			&& strncmp(lex->buffer_pos, t->token.literal.str, t->token.literal.len) == 0) {
-			
+
 			_llex_set_location(lex, lex->buffer_pos, t->token.literal.len);
-				
+
 			lex->current_token = lex->buffer_pos;
 			lex->current_len = t->token.literal.len;
 			lex->buffer_pos += t->token.literal.len;
-					
+
 			if (t->type == LLEX_LITERAL_TOKEN_CALLBACK) {
 				return t->data.callback(lex, lex->current_token, lex->current_len);
 			}
-			
+
 			return t->data.id;
 		} else if (t->type == LLEX_REGEX_TOKEN || t->type == LLEX_REGEX_TOKEN_CALLBACK) {
 			char *buffer_pos = lex->buffer_pos;
+
 			if (llex_regex_match(lex, t->token.regex, lex->buffer_pos) == 0) {
-				if (t->type == LLEX_REGEX_TOKEN_CALLBACK) {					
-					int r = t->data.callback(lex, lex->current_token, lex->current_len);										
+				if (t->type == LLEX_REGEX_TOKEN_CALLBACK) {
+					int r = t->data.callback(lex, lex->current_token, lex->current_len);
+
 					_llex_set_location(lex, buffer_pos, lex->buffer_pos - buffer_pos);
-					return r;					
+					return r;
 				}
 				_llex_set_location(lex, buffer_pos, lex->buffer_pos - buffer_pos);
 				return t->data.id;
@@ -97,6 +99,6 @@ inline static int _llex_find_next_token(llex *lex)
 }
 
 llex_token_id llex_tokenizer(llex *lex)
-{	
+{
 	return _llex_find_next_token(lex);
 }
